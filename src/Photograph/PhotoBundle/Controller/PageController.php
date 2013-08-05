9f94c392c252d94a3480cf842db21d6872f55963
sam <sam@sam-virtual-machine>
2013-08-05 20:55:21 +0200

    test

--- src/Photograph/PhotoBundle/Controller/PageController.php
+++ src/Photograph/PhotoBundle/Controller/PageController.php
@@ -4,15 +4,30 @@ namespace Photograph\PhotoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class PageController extends Controller
-{
-    public function contactAction()
-    {
+class PageController extends Controller {
+
+    public function contactAction() {
         return $this->render('PhotographPhotoBundle:Page:contact.html.twig');
     }
-    
-    public function portfolioAction()
-    {
+
+    public function portfolioAction() {
         return $this->render('PhotographPhotoBundle:Page:portfolio.html.twig');
     }
+
+    public function aboutAction() {
+        return $this->render('PhotographPhotoBundle:Page:about.html.twig');
+    }
+
+    public function faqAction() {
+        return $this->render('PhotographPhotoBundle:Page:faq.html.twig');
+    }
+
+    public function sitemapAction() {
+        return $this->render('PhotographPhotoBundle:Page:sitemap.html.twig');
+    }
+
+    public function galleryAction() {
+        return $this->render('PhotographPhotoBundle:Page:gallery.html.twig');
+    }
+
 }
